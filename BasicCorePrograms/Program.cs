72cccd9d3065a66e4898bdefc12caff727c7296e
Anant1202 <anantsingh1202@gmail.com>
2022-05-08 17:33:07 +0530

    Added:Leap Year Program

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -7,8 +7,10 @@ namespace BasicCorePrograms
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Programs");
-            FlipCoin obj1= new FlipCoin();
-            obj1.Percentage();
+            //FlipCoin obj1= new FlipCoin();
+            //obj1.Percentage();
+            LeapYear ob2 = new LeapYear();
+            ob2.checkingLeapYear();
 
         }
 
