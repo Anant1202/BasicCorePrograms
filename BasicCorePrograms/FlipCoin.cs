9f61f28fdccdb8daa5d029e130f8f8dfc005158b
Anant1202 <anantsingh1202@gmail.com>
2022-05-08 16:54:45 +0530

    Added:Flip Coin Program

---
+++ BasicCorePrograms/FlipCoin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class FlipCoin
+    {
+        public void Percentage()
+        {
+            //Enter a positive integer
+            Console.WriteLine("Enter the number of times to Flip a coin");
+            int num=Convert.ToInt32(Console.ReadLine());
+           
+            double x=0, y=0;
+            for (int i = 0; i < num; i++)
+            {
+                Random random = new Random();
+                double getnum = random.NextDouble();
+                Console.WriteLine("No.:"+getnum);
+                if (getnum < 0.5)
+                {
+                    Console.WriteLine("Tails");
+                    x++;
+                }
+                else
+                {
+                    Console.WriteLine("Heads");
+                    y++;
+                }
+            }
+            double Tail = x;
+            double Per = Tail * 100;
+            double Tailper = Per / num;
+            Console.WriteLine("Tail percentage:" + Tailper);
+            double Head = y;
+            double Per1 = Head * 100;
+            double Headper = Per1 / num;
+            Console.WriteLine("Head percentage:" + Headper);
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -7,6 +7,10 @@ namespace BasicCorePrograms
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Programs");
+            FlipCoin obj1= new FlipCoin();
+            obj1.Percentage();
+
         }
+
     }
 }
