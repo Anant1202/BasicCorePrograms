76a4aa512460c428917516a7b1d6dfeb92da1877
Anant1202 <anantsingh1202@gmail.com>
2022-05-08 15:50:31 +0530

    Welcome to Basic Core Programs

---
+++ BasicCorePrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicCorePrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Programs");
+        }
+    }
+}
