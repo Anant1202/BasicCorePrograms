df4aa8284e6fdc05e6b8698d1ea8e095951a3773
Anant1202 <anantsingh1202@gmail.com>
2022-05-09 17:23:45 +0530

    Added:Largest Number Program

---
+++ BasicCorePrograms/Largest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class Largest
+    {
+        public void LargestNum()
+        {
+            Console.WriteLine("Enter the numbers");
+            Console.Write("num1:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("num2:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("num3:");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            if(num1>num2 && num1>num3)
+            {
+                Console.WriteLine("Num1:{0} is largest", num1);
+            }
+            else if(num2>num1 && num2>num3)
+            {
+                Console.WriteLine("Num2:{0} is largest",num2);
+            }
+            else
+            {
+                Console.WriteLine("Num3:{0} is largest", num3);
+            }
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -7,26 +7,51 @@ namespace BasicCorePrograms
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Programs");
-            //FlipCoin obj1= new FlipCoin();
-            //obj1.Percentage();
-            //LeapYear ob2 = new LeapYear();
-            //ob2.checkingLeapYear();
-          //Powerof2 ob3 = new Powerof2();
-          //ob3.calculatingPower();
-            //HarmonicNumber ob4=new HarmonicNumber();
-            //ob4.harmonicValue();
-            //PrimeFactor ob5 = new PrimeFactor();
-            //ob5.findingPrimeFactors();
-            //QuotientAndRemainder ob6= new QuotientAndRemainder();
-            //ob6.Compute();
-            //Swap ob7= new Swap();
-            //ob7.swapTwoNumber();
-            //EvenOdd ob8= new EvenOdd();
-            //ob8.check();
-            VowelOrConsonant ob9=new VowelOrConsonant();
-            ob9.checkAlphabet();
-
+            Console.WriteLine("Enter number between 1 to 10 to check Programs");
+            int options = Convert.ToInt32(Console.ReadLine());
+            switch (options)
+            {
+                case 1:
+                    FlipCoin obj1 = new FlipCoin();
+                    obj1.Percentage();
+                    break;
+                case 2:
+                    LeapYear ob2 = new LeapYear();
+                    ob2.checkingLeapYear();
+                    break;
+                case 3:
+                    Powerof2 ob3 = new Powerof2();
+                    ob3.calculatingPower();
+                    break;
+                case 4:
+                    HarmonicNumber ob4 = new HarmonicNumber();
+                    ob4.harmonicValue();
+                    break;
+                case 5:
+                    PrimeFactor ob5 = new PrimeFactor();
+                    ob5.findingPrimeFactors();
+                    break;
+                case 6:
+                    QuotientAndRemainder ob6 = new QuotientAndRemainder();
+                    ob6.Compute();
+                    break;
+                case 7:
+                    Swap ob7 = new Swap();
+                    ob7.swapTwoNumber();
+                    break;
+                case 8:
+                    EvenOdd ob8 = new EvenOdd();
+                    ob8.check();
+                    break;
+                case 9:
+                    VowelOrConsonant ob9 = new VowelOrConsonant();
+                    ob9.checkAlphabet();
+                    break;
+                case 10:
+                    Largest ob10 = new Largest();
+                    ob10.LargestNum();
+                    break;
+            }
         }
-
     }
 }
